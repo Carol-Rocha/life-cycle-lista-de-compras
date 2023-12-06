8d731f626c5bfcd7649623001e1953d42b7d87bb
Carol-Rocha <caroldsr1@gmail.com>
2023-12-06 15:11:20 -0300

    feat: create function to edit an item

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,12 +13,31 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemEditado!: Item;
+
+  editando = false;
+  textoBtn = 'Salvar item';
   valorItem!: string;
 
   constructor(private listaService: ListaDeCompraService) {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['itemEditado'].firstChange) {
+      this.editando = true;
+      this.textoBtn = 'Editar item';
+      this.valorItem = this.itemEditado?.nome;
+    }
+  }
+
+  editarItem() {
+    this.listaService.editarItemDaLista(this.itemEditado, this.valorItem);
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = 'Salvar item';
+  }
 
   adicionarItem() {
     this.listaService.adicionarItemNaLista(this.valorItem);
